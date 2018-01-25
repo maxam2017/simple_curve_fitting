b4eaa04bad63ad0a88afbf99e443df3c85f7c03d
maxam2017 <whk0313@gmail.com>
2018-01-25 23:17:17 +0800

    Add files via upload

---
+++ test_2.m
@@ -0,0 +1,12 @@
+clear;
+clc;
+hold on;
+x = zeros(2,100);
+p=0:0.01:1;
+[x(1,:), x(2,:)] = textread('Dateset2.txt','%f%f','delimiter',',','headerlines', 1);
+A=(8/(2*pi)^0.5)*[gaussmf(x(1,:),[0.125 1]) ;gaussmf(x(1,:),[0.125 0.875]);gaussmf(x(1,:),[0.125 0.750]);gaussmf(x(1,:),[0.125 0.625]);gaussmf(x(1,:),[0.125 0.500]);gaussmf(x(1,:),[0.125 0.375]);gaussmf(x(1,:),[0.125 0.250]);gaussmf(x(1,:),[0.125 0.125]);gaussmf(x(1,:),[0.125 0])];
+b=[x(2,:)'];
+W=inv(A*A')*A*b;
+q=W(1)*gaussmf(p,[0.125 1])+W(2)*gaussmf(p,[0.125 0.875])+W(3)*gaussmf(p,[0.125 0.750])+W(4)*gaussmf(p,[0.125 0.625])+W(5)*gaussmf(p,[0.125 0.500])+W(6)*gaussmf(p,[0.125 0.375])+W(7)*gaussmf(p,[0.125 0.250])+W(8)*gaussmf(p,[0.125 0.125])+W(9)*gaussmf(p,[0.125 0]);
+plot(p,q )
+plot(x(1,:), x(2,:),'.')
---
+++ test_3.m
@@ -0,0 +1,18 @@
+clear;
+clc;
+hold on;
+x = zeros(2,100);
+p=0:0.01:1;
+[x(1,:), x(2,:)] = textread('Dateset1.txt','%f%f','delimiter',',','headerlines', 1);
+A=[power(x(1,:),8);power(x(1,:),7);power(x(1,:),6);power(x(1,:),5);power(x(1,:),4);power(x(1,:),3);power(x(1,:),2);power(x(1,:),1);power(x(1,:),0)];
+b=[x(2,:)'];
+W=inv(A*A')*A*b;
+q=W(1).*p.^8+W(2).*p.^7+W(3).*p.^6+W(4).*p.^5+W(5).*p.^4+W(6).*p.^3+W(7).*p.^2+W(8).*p.^1+W(9);
+plot(p,q)
+plot(x(1,:), x(2,:),'.')
+e=A'*W-b;
+sum=0;
+for i=1:100
+    sum=sum+e(i)^2;
+end
+sum=sum/100;
